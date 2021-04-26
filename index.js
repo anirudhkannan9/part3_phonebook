e76dc1ba1a2398a466ceeae849e38d8fc4a77837
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-04-25 18:36:32 -0700

    Ex 3.2: phonebook backend step 2

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    var info = `Phonebook has info for ${persons.length} people` 
+    var date = `<br/> <br/> ${new Date()}`
+    response.send(info + date)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
